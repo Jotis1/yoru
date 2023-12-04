9087aa784dc9cc69a17c4cbb320a04c04597ab01
Jotis <74616896+Jotis1@users.noreply.github.com>
2023-12-04 21:38:04 +0100

    File Error solved

--- app/(welcome)/action.ts
+++ app/(welcome)/action.ts
@@ -3,25 +3,15 @@
 import { UserSchema, UserProps } from "@/app/schema";
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 export async function loginWithEmailAndPassword(prevState: any, formdata: FormData) {
   try {
 
-    const emailField = formdata.get("email");
-    const passwordField = formdata.get("password");
-
-
-    if (!emailField || !passwordField) throw new Error("No puede haber campos vacíos");
-    if (emailField instanceof File || passwordField instanceof File) throw new Error("El campo no puede ser un archivo");
-
-    console.log(emailField + passwordField);
-
-    let email: string = emailField;
-    let password: string = passwordField;
+    const emailField: any = formdata.get("email");
+    const passwordField: any = formdata.get("password");
 
     const userURL: URL = new URL("https://656e112cbcc5618d3c247c75.mockapi.io/api/v1/users");
-    userURL.searchParams.append("email", email);
+    userURL.searchParams.append("email", emailField);
 
     const response: Response = await fetch(userURL);
 
@@ -34,7 +24,7 @@ export async function loginWithEmailAndPassword(prevState: any, formdata: FormDa
     UserSchema.parse(data[0]);
     const userData: UserProps = data[0]
 
-    if (userData.password !== password) throw new Error("Las contraseñas no coinciden");
+    if (userData.password !== passwordField) throw new Error("Las contraseñas no coinciden");
 
     revalidatePath("/login");
 
