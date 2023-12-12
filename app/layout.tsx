87849a36f23dec0ed143ffa1f167ff891d6d18fc
Jotis <74616896+Jotis1@users.noreply.github.com>
2023-12-12 22:19:48 +0100

    Speed insights updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import type { Metadata } from 'next';
 
@@ -24,7 +24,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
